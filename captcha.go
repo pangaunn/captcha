19ce0b5b42eb3dd5ea820b38f19ad9443e187912
Pangaunn <aunn_it08@hotmail.com>
2019-01-05 14:30:30 +0700

    add case 1 1 2 1

--- captcha.go
+++ captcha.go
@@ -1,5 +1,8 @@
 package captcha
 
-func Captcha(pattern, op1, op2, operator int) string {
+func Captcha(pattern, op1, operator, op2 int) string {
+	if operator == 2 {
+		return "1 - one"
+	}
 	return "1 + one"
 }
--- captcha_test.go
+++ captcha_test.go
@@ -13,3 +13,11 @@ func TestCaptcha1PlusOne(t *testing.T) {
 		t.Errorf("It should say %s but get %s", expected, result)
 	}
 }
+
+func TestCaptcha1MinusOne(t *testing.T) {
+	expected := "1 - one"
+	result := captcha.Captcha(1, 1, 2, 1)
+	if expected != result {
+		t.Errorf("It should say %s but get %s", expected, result)
+	}
+}
